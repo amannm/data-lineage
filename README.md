8a4c65ef7536919130d07c969af2cc37a887f8ec
Amann Malik <amannmalik@gmail.com>
2019-08-03 14:01:37 -0500

    add more tests and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # language
+
+use ANTLR4 to extract table references from a SQLite input script
--- src/test/java/systems/cauldron/service/language/MainTest.java
+++ src/test/java/systems/cauldron/service/language/MainTest.java
@@ -13,9 +13,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MainTest {
 
     @Test
-    void test() {
+    void test1() {
         Set<TableReference> extract = Main.extract("SELECT hello.test, world.fart FROM hello.world INNER JOIN foo.bar on world.asdf = bar.asdf");
         assertEquals(new HashSet<>(Arrays.asList("hello.world", "foo.bar")), extract.stream().map(TableReference::toString).collect(Collectors.toSet()));
+    }
+
+
+    @Test
+    void test2() {
+        Set<TableReference> extract = Main.extract("SELECT hello.test, world.fart FROM hello INNER JOIN foo.bar on world.asdf = bar.asdf");
+        assertEquals(new HashSet<>(Arrays.asList("hello", "foo.bar")), extract.stream().map(TableReference::toString).collect(Collectors.toSet()));
+    }
+
+    @Test
+    void test3() {
+        Set<TableReference> extract = Main.extract("SELECT hello.test, world.fart FROM hello.world INNER JOIN foo on world.asdf = bar.asdf");
+        assertEquals(new HashSet<>(Arrays.asList("hello.world", "foo")), extract.stream().map(TableReference::toString).collect(Collectors.toSet()));
+    }
+
+
+    @Test
+    void test4() {
+        Set<TableReference> extract = Main.extract("SELECT hello.test, world.fart FROM world INNER JOIN foo on world.asdf = bar.asdf");
+        assertEquals(new HashSet<>(Arrays.asList("world", "foo")), extract.stream().map(TableReference::toString).collect(Collectors.toSet()));
     }
 
 }
